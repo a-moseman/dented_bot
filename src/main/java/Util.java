076f54585e6394c6fed222166dc2b3a9678b9989
a-moseman <drewmoseman@gmail.com>
2022-02-02 16:29:23 -0500

    Implement more proper command parsing.

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Util {
     public static String convertToReadableTime(Long nanoSeconds){
         long tempSec = nanoSeconds / 1000000000;
@@ -6,6 +8,28 @@ public class Util {
         long hour = (tempSec /(60 * 60)) % 24;
         long day = (tempSec / (24 * 60 * 60)) % 24;
         return String.format("%dd %dh %dm %ds", day, hour, min, sec);
+    }
 
+    public static String[] split(String string) {
+        ArrayList<String> contents = new ArrayList<>();
+        boolean inQuotes = false;
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '"') {
+                if (inQuotes) {
+                    contents.add(content.toString());
+                    content = new StringBuilder();
+                }
+                inQuotes = !inQuotes;
+            }
+            else if (!inQuotes && string.charAt(i) == ' ') {
+                contents.add(content.toString());
+                content = new StringBuilder();
+            }
+            else {
+                content.append(string.charAt(i));
+            }
+        }
+        return contents.toArray(new String[0]);
     }
 }
