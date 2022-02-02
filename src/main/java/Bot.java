e8b7bcdd559bcea9bb1c6757619d9f8fbcf07729
a-moseman <drewmoseman@gmail.com>
2022-02-02 12:37:36 -0500

    Add $help command.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -13,7 +13,9 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Bot {
-    private double CHANCE_OF_RANDOM_RESPONSE;
+    private final char COMMAND_CHARACTER = '$';
+
+    private final double CHANCE_OF_RANDOM_RESPONSE;
 
     private ArrayList<String> responses;
 
@@ -65,8 +67,8 @@ public class Bot {
     }
 
     public String getBotResponse(String userMessage) {
-        if (userMessage.charAt(0) == '$') {
-            return getCommandResponse(userMessage);
+        if (userMessage.charAt(0) == COMMAND_CHARACTER) {
+            return getCommandResponse(userMessage.substring(1));
         }
         else if (random.nextDouble() < CHANCE_OF_RANDOM_RESPONSE) {
             return responses.get(random.nextInt(responses.size()));
@@ -75,13 +77,19 @@ public class Bot {
     }
 
     private String getCommandResponse(String userMessage) {
-        if (userMessage.equals("$info")) {
-            return "Info:\n" +
-                    "Current Runtime: " + (Util.convertToReadableTime(System.nanoTime() - startNanoTime)) + "\n" +
-                    "Language: Java 11\n" +
-                    "Libraries: JDA\n" +
-                    "Author: Andrew Moseman\n";
+        switch (userMessage) {
+            case "help":
+                return "Commands:\n" +
+                        "$help - get a list of commands\n" +
+                        "$info - get bot information and diagnostics";
+            case "info":
+                return "Info:\n" +
+                        "Current Runtime: " + (Util.convertToReadableTime(System.nanoTime() - startNanoTime)) + "\n" +
+                        "Language: Java 11\n" +
+                        "Libraries: JDA\n" +
+                        "Author: Andrew Moseman\n";
+            default:
+                return userMessage + " is not a valid command.";
         }
-        return userMessage + " is not a valid command.";
     }
 }
