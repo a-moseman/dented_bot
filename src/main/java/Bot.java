718e2dd0fe421b1058f95609b97e85cd5e376c2a
a-moseman <drewmoseman@gmail.com>
2022-02-07 16:39:26 -0500

    Refactor code to be more object oriented.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -51,15 +51,15 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    public String[] getBotResponse(String userMessage, String authorUUID) {
+    public BotResponse getBotResponse(String userMessage, String authorUUID) {
         char COMMAND_CHARACTER = '$';
         if (userMessage.charAt(0) == COMMAND_CHARACTER) {
             return doCommand(Util.split(userMessage.substring(1)), authorUUID);
         }
         else if (RANDOM.nextDouble() < CHANCE_OF_RANDOM_RESPONSE) {
-            return new String[]{RESPONSES.get(RANDOM.nextInt(RESPONSES.size()))};
+            return new BotResponse(new String[]{RESPONSES.get(RANDOM.nextInt(RESPONSES.size()))});
         }
-        return new String[]{};
+        return new BotResponse(new String[]{});
     }
 
     /**
@@ -68,7 +68,7 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] doCommand(String[] command, String authorUUID) {
+    private BotResponse doCommand(String[] command, String authorUUID) {
         switch (command[0]) {
             case "help":
                 return help(command, authorUUID);
@@ -77,7 +77,7 @@ public class Bot {
             case "survey":
                 return survey(command, authorUUID);
             default:
-                return new String[]{"$" + String.join(" ", command) + " is not a valid command."};
+                return new BotResponse(new String[]{"$" + String.join(" ", command) + " is not a valid command."});
         }
     }
 
@@ -87,15 +87,15 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] help(String[] command, String authorUUID) {
-        return new String[]{"Commands:\n" +
+    private BotResponse help(String[] command, String authorUUID) {
+        return new BotResponse(new String[]{"Commands:\n" +
                 "$help - get a list of commands\n" +
                 "$info - get bot information and diagnostics\n" +
                 "$survey open <name> <choices> - open a survey\n" +
                 "$survey close - close your survey\n\n" +
                 "Notes:\n" +
                 "Lists must be contained in quotes with the elements separated by only commas.\n" +
-                "Spaces are seen as delimiters unless within quotes."};
+                "Spaces are seen as delimiters unless within quotes."});
     }
 
     /**s
@@ -104,12 +104,12 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] info(String[] command, String authorUUID) {
-        return new String[]{"Info:\n" +
+    private BotResponse info(String[] command, String authorUUID) {
+        return new BotResponse(new String[]{"Info:\n" +
                 "Runtime: " + (Util.convertToReadableTime(System.nanoTime() - START_NANO_TIME)) + "\n" +
                 "Language: Java 11\n" +
                 "Discord API Wrapper: JDA\n" +
-                "Author: Andrew Moseman\n"};
+                "Author: Andrew Moseman\n"});
     }
 
     /**
@@ -118,14 +118,14 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] survey(String[] command, String authorUUID) {
+    private BotResponse survey(String[] command, String authorUUID) {
         switch (command[1]) {
             case "open":
                 return surveyOpen(command, authorUUID);
             case "close":
                 return surveyClose(command, authorUUID);
             default:
-                return new String[]{"$" + String.join(" ", command) + " is not a valid command."};
+                return new BotResponse(new String[]{"$" + String.join(" ", command) + " is not a valid command."});
         }
     }
 
@@ -135,14 +135,11 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] surveyOpen(String[] command, String authorUUID) {
+    private BotResponse surveyOpen(String[] command, String authorUUID) {
         String[] choices = command[3].split(",");
-        messageListener.sendingSurveyMessage = true;
-        messageListener.newSurveyName = command[2];
-
         ArrayList<String> tempList = new ArrayList<String>(Arrays.asList(choices));
-        tempList.add(messageListener.newSurveyName);
-        return tempList.toArray(new String[0]);
+        tempList.add(command[2]);
+        return new BotResponse(tempList.toArray(new String[0])).setAsSurvey(command[2]);
     }
 
     /**
@@ -151,12 +148,12 @@ public class Bot {
      * @param authorUUID
      * @return
      */
-    private String[] surveyClose(String[] command, String authorUUID) {
+    private BotResponse surveyClose(String[] command, String authorUUID) {
         // TODO: optimize
         Hashtable<String, Survey> surveys = messageListener.surveys;
         Hashtable<String, ArrayList<String>> questions = surveys.get(authorUUID).getQuestionsAndVotes();
         if (questions == null) {
-            return new String[]{"You do not have an open survey"};
+            return new BotResponse(new String[]{"You do not have an open survey"});
         }
         Iterator<String> iter = questions.keys().asIterator();
         ArrayList<String> questionNames = new ArrayList<>();
@@ -185,6 +182,6 @@ public class Bot {
             response[i + 1] = topVoted.get(i).replaceFirst(authorUUID, "") + " - " + questions.get(topVoted.get(i)).size();
         }
         surveys.remove(authorUUID); //close the survey
-        return response;
+        return new BotResponse(response);
     }
 }
